76abdffd29f9be04dda348cfc5acd1e7c6a334e8
David Cooper <dazecoop@gmail.com>
2021-10-19 10:21:10 +0100

    store

--- store/index.js
+++ store/index.js
@@ -5,16 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    autologin_url: '',
+    path: '',
   },
   mutations: {
-    setAutoLoginUrl(state, url) {
-      state.autologin_url = url;
+    setPath(state, val) {
+      state.path = val;
     },
   },
   getters: {
-    autoLoginUrl(state) {
-      return state.autologin_url;
+    getPath(state) {
+      return state.path;
     },
   },
 })
