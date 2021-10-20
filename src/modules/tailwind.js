ffc66f0107bd26ec9e8aaf0415d5e3c3143db4d0
David Cooper <dazecoop@gmail.com>
2021-10-20 12:53:47 +0100

    Form & table styling + demos

--- src/modules/tailwind.js
+++ src/modules/tailwind.js
@@ -26,40 +26,18 @@ import {
   // TDialog,
 } from 'vue-tailwind/dist/components'
 
-// Class's applied to any interactive element
-const interactiveFocus = 'focus:outline-none focus:ring focus:border-blue-300';
-
-// Generic class's for all form elements
-const formClasses = 'w-full rounded border-gray-400' +
-  ' focus:outline-none focus:ring focus:border-blue-300' +
-  interactiveFocus
-
 const settings = {
   't-button': {
     component: TButton,
-    props: {
-      classes: 'btn bg-red-500 hover:bg-red-700 text-white font-bold py-3' +
-        ' px-4 w-100 rounded w-full uppercase drop-shadow-md ' +
-        interactiveFocus,
-    }
   },
   't-input': {
     component: TInput,
-    props: {
-      classes: formClasses
-    }
   },
   't-textarea': {
     component: TTextarea,
-    props: {
-      classes: formClasses
-    }
   },
   't-select': {
     component: TSelect,
-    props: {
-      classes: formClasses
-    }
   }
 }
 
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,23 +1,110 @@
 <template>
-  <div class="text-center p-8">
-    <h1 class="text-3xl pb-5">
-      {{ $t(`${localePath}.intro.title`, { siteName: $siteName }) }}
-    </h1>
-    <p>
-      {{ $t(`${localePath}.intro.p-1`) }}
-    </p>
-    <p>
-      <a
-        :href="$siteDomain"
-        class="hover:text-blue-500"
-      >
-        <font-awesome-icon
-          :icon="['fab', 'github']"
-          class="mr-1"
+  <!-- eslint-disable max-len -->
+  <div class="p-8">
+    <div class="text-center">
+      <h1>
+        {{ $t(`${localePath}.intro.title`, { siteName: $siteName }) }}
+      </h1>
+      <p class="text-lg font-light">
+        {{ $t(`${localePath}.intro.p-1`) }}
+      </p>
+      <p class="text-sm">
+        <a
+          :href="$siteDomain"
+          class="hover:text-blue-500 p-2"
+        >
+          <font-awesome-icon
+            :icon="['fab', 'github']"
+            class="mr-1"
+          />
+          {{ $siteDomain }}
+        </a>
+      </p>
+    </div>
+
+    <hr class="my-8" />
+
+    <form class="max-w-md mx-auto">
+      <h2>Form example</h2>
+
+      <fieldset>
+        <label for="example-1">
+          Regular input
+        </label>
+        <t-input
+          id="example-1"
+          placeholder="Input placeholder"
         />
-        {{ $siteDomain }}
-      </a>
-    </p>
+      </fieldset>
+
+      <fieldset>
+        <label for="example-2">
+          Has an error
+        </label>
+        <t-input
+          id="example-2"
+          class="error"
+          placeholder="Input placeholder"
+        />
+        <div class="alert error mt-1">
+          Please enter a value
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <label for="example-3">
+          Input with prepend
+        </label>
+        <div class="prepend">
+          <span>
+            http://
+          </span>
+          <t-input
+            id="example-3"
+            placeholder="Input placeholder"
+            class="rounded-l-none"
+          />
+        </div>
+      </fieldset>
+
+      <fieldset>
+        <div class="grid sm:grid-cols-3 gap-4">
+          <t-button>
+            Submit
+          </t-button>
+          <t-button
+            class="loading"
+            disabled
+          >
+            Loading
+          </t-button>
+        </div>
+      </fieldset>
+    </form>
+
+    <hr class="my-8" />
+
+    <div class="max-w-md mx-auto">
+      <h2>Table example</h2>
+      <table class="table stripe">
+        <thead>
+          <tr>
+            <th>Header 1</th>
+            <th>Header 2</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Val 1</td>
+            <td>Val 2</td>
+          </tr>
+          <tr>
+            <td>Val 1.1</td>
+            <td>Val 2.1</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
