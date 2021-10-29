9efc3dac0aeef527a3d4475150472e17c525bf69
David Cooper <dazecoop@gmail.com>
2021-10-29 10:07:57 +0100

    validateEmail filter

--- src/modules/filters.js
+++ src/modules/filters.js
@@ -5,3 +5,9 @@ var numeral = require('numeral');
 Vue.filter('formatNumber', function (value) {
   return numeral(value).format('0,0');
 });
+
+Vue.filter('validateEmail', function(email) {
+  // eslint-disable-next-line max-len
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+})
